1746d6f76096ade0551425280428c7e5576b69f9
agilb884 <arthur.gilbert@emc.com>
2018-09-13 21:22:23 +0000

    working version with NodeJs

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -19,14 +19,23 @@ getPosts() {
   });
 //  return [...this.posts];
 }
+
+
 getPostUpdateListener() {
   return this.postsUpdated.asObservable();
 }
 
 addPost(title: string, content: string) {
   const post: Post = {id: null, title: title, content: content};
+  this.httpClient.post<{message: string}>('http://localhost:3000/api/posts', post)
+  .subscribe((responseData) => {
+     console.log(responseData.message);
+     this.posts.push(post);
+     this.postsUpdated.next([...this.posts]);
+   });
+
   this.posts.push(post);
   this.postsUpdated.next([...this.posts]);
-}
+ }
 
 }
